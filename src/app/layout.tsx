3746613dd184889032ecf85f6b243981ca27e68a
Gonza-Butafuoco <butafuocogonzalo@gmail.com>
2024-07-19 22:06:45 -0300

    header and footer update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { montserrat } from "../UI/fonts";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,57 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <header
+        className={`${montserrat.className} antialiased w-full h-24 bg-blue-600 rounded-sm border-b border-black shadow-lg `}
+      >
+        <div className="flex justify-between  h-full ">
+          <div className="flex items-center pl-2">
+            <Link href={"/"}>
+            <div className="flex items-center gap-2">
+              <h1 className="text-white text-xl md:text-3xl">Translate PDF</h1>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-8  gap-5 text-white"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                />
+              </svg>
+
+            </div>
+            </Link>
+          </div>
+          <div className="flex  items-center gap-4 pr-2">
+            <Link href={"/"}>
+              <h2 className="text-white text-lg md:text-2xl ">Translate</h2>
+            </Link>
+            <h2 className="text-white text-lg md:text-2xl ">About Us</h2>
+            <h2 className="text-white text-lg md:text-2xl ">Contact</h2>
+          </div>
+        </div>
+      </header>
+      <body className={`${montserrat.className} antialiased`}>{children}</body>
+      <footer
+        className={`${montserrat.className} antialiased w-full h-24 bg-blue-600 rounded-sm border-t border-black shadow-lg `}
+      >
+        <div className="flex justify-between h h-full">
+          <div className="flex justify-start items-center pl-2">
+            <p className="text-white">
+              © 2024 Translate PDF. All rights Reserved.{" "}
+            </p>
+          </div>
+          <div className="flex items-center gap-4 pr-2">
+            <p className="text-white">Privacy Policy </p>
+            <p className="text-white">Terms of Service</p>
+          </div>
+        </div>
+      </footer>
     </html>
   );
 }
